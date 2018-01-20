702f2f7348d5eb9748bc76fe5cd790d8c354f8ae
Emerson Rocha Luiz <emerson@alligo.com.br>
2018-01-20 03:08:55 -0200

    Installed docker-compose; copied traefik & rocketchat based on EticaAI/eticaai-infrastructure

--- logbook/chatopswg.sh
+++ logbook/chatopswg.sh
@@ -5,11 +5,12 @@
 
 # ssh root@45.55.32.60
 # ssh root@chatopswg.alligo.com.br
+# mosh root@chatopswg.alligo.com.br
 
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install htop tree jq -y # Not really necessary, but I, Rocha, like it on host VM
+sudo apt install htop tree jq mosh -y # Not really necessary, but I, Rocha, like it on host VM
 
 ############################# **Security warning** #############################
 # This VM is designed for testing and be friendly even for who is new to docker
@@ -26,8 +27,22 @@ sudo apt install htop tree jq -y # Not really necessary, but I, Rocha, like it o
 #
 # For running docker on production or with more sensive data, consider
 # this documentation at https://docs.docker.com/engine/security/security/
-################################################################################
+############################# **Security warning** #############################
 
-## Install docker on ubuntu
+### Install docker on ubuntu
 # See https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-16-04
 # See (in Portuguese) https://www.digitalocean.com/community/tutorials/como-instalar-e-usar-o-docker-no-ubuntu-16-04-pt
+
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+sudo apt-get update
+sudo apt-get install -y docker-engine
+
+### Install Docker compose
+# See https://docs.docker.com/compose/install/#install-compose
+
+# change X.XX.X based on last release of https://github.com/docker/compose/releases
+dockerComposeVersion=1.18.0
+
+sudo curl -L https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
