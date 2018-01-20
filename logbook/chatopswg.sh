6cde0d7d460a0d117196eb8ead916857343afdfb
Emerson Rocha Luiz <emerson@alligo.com.br>
2018-01-20 02:46:41 -0200

    Added security warning

--- logbook/chatopswg.sh
+++ logbook/chatopswg.sh
@@ -5,34 +5,28 @@
 
 # ssh root@45.55.32.60
 # ssh root@chatopswg.alligo.com.br
-## Recomended:
-# ssh ubuntu@chatopswg.alligo.com.br
 
 sudo apt update
 sudo apt upgrade -y
 
 sudo apt install htop tree jq -y # Not really necessary, but I, Rocha, like it on host VM
 
-### Initial serversetup
-## See https://www.digitalocean.com/community/tutorials/initial-server-setup-with-ubuntu-16-04
-
-# Add user 'ubuntu' with sudo powers
-adduser ubuntu
-usermod -aG sudo ubuntu
-
-sudo su ubuntu # log as ubuntu user
-cd ~
-
-# Generate keygen, as ubuntu user
-ssh-keygen
-
-vim /home/ubuntu/.ssh/authorized_keys # then paste your public key from **local* machine, e.g. `cat ~/.ssh/id_rsa.pub` on this file
-chmod 600 /home/ubuntu/.ssh/authorized_keys
-exit # exited to root
-exit # exited from VM
-
-# Now log as non-root user
-ssh ubuntu@chatopswg.alligo.com.br
+############################# **Security warning** #############################
+# This VM is designed for testing and be friendly even for who is new to docker
+# at least start project without care much about running docker on production.
+# And the lifetime of this VM will be less than 6 months. And, by other factors
+# than just security, will have some regular backups or easy to be regenerated.
+# This explain why docker will be running as root, which is less secure, because
+# is faster to avoid extra steps and a a lot of time-consuming debugging. PRs
+# and issues on GitHub page are wellcome, in special if some configuration
+# on this infrastructure could easily exploited by automated tools, but just
+# consider that some even less secure use on the containers, like explicit open
+# ports or admin panels, are not just because is a testing envoriment, but
+# to make it more frienly to non devops people.
+#
+# For running docker on production or with more sensive data, consider
+# this documentation at https://docs.docker.com/engine/security/security/
+################################################################################
 
 ## Install docker on ubuntu
 # See https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-16-04
